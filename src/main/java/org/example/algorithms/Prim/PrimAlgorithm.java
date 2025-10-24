6f6cc5ba5c1c1795878d1f84295e9afe53b21dd8
Mansurrv <buyserikov06@gmail.com>
2025-10-24 18:08:37 +0500

    (init): small, medium, extra graph data's

--- src/main/java/org/example/algorithms/Prim/PrimAlgorithm.java
+++ src/main/java/org/example/algorithms/Prim/PrimAlgorithm.java
@@ -1,27 +1,30 @@
 package org.example.algorithms.Prim;
 
 import com.google.gson.Gson;
-
 import java.io.FileReader;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PrimAlgorithm {
-    static class Edge{
-        String from;
-        String to;
+    static class Node {
+        int id;
+    }
+
+    static class Edge {
+        int source;
+        int target;
         int weight;
     }
 
-    static class Graph{
-        int id;
-        String[] nodes;
+    static class GraphData {
+        List<Node> nodes;
         List<Edge> edges;
     }
 
-    static class GraphsWrapper{
-        List<Graph> graphs;
+    static class Root {
+        Map<String, Object> properties;
+        GraphData graph;
     }
 
     int min(int[] key, Boolean[] set) {
@@ -29,8 +32,8 @@ public class PrimAlgorithm {
         int min_idx = -1;
         int len = set.length;
 
-        for(int i=0; i<len; i++) {
-            if(!set[i] && key[i]<min){
+        for (int i = 0; i < len; i++) {
+            if (!set[i] && key[i] < min) {
                 min = key[i];
                 min_idx = i;
             }
@@ -38,23 +41,21 @@ public class PrimAlgorithm {
         return min_idx;
     }
 
-
-    void print(int[] parent, int[][] graph, String[] nodes){
+    void print(int[] parent, int[][] graph) {
         System.out.println("Edge \tWeight");
-        int len = graph.length;
-        for(int i=0; i<len; i++){
-            if(parent[i] != -1) System.out.println(nodes[parent[i]] + " - " + nodes[i] + "\t" + graph[parent[i]][i]);
+        for (int i = 1; i < graph.length; i++) {
+            if (parent[i] != -1)
+                System.out.println(parent[i] + " - " + i + "\t" + graph[parent[i]][i]);
         }
     }
 
-
-    void MST(int[][] graph, String[] nodes){
+    void MST(int[][] graph) {
         int V = graph.length;
-        int[] parent  = new int[V];
-        int[] key     = new int[V];
+        int[] parent = new int[V];
+        int[] key = new int[V];
         Boolean[] set = new Boolean[V];
 
-        for(int i=0; i<V; i++){
+        for (int i = 0; i < V; i++) {
             key[i] = Integer.MAX_VALUE;
             set[i] = false;
         }
@@ -62,54 +63,58 @@ public class PrimAlgorithm {
         key[0] = 0;
         parent[0] = -1;
 
-        for(int count=0; count<V; count++){
-            int u  = min(key, set);
+        for (int count = 0; count < V - 1; count++) {
+            int u = min(key, set);
             set[u] = true;
 
-            for(int v=0; v<V; v++){
-                if(graph[u][v]!=0 && !set[v] && graph[u][v]<key[v]){
+            for (int v = 0; v < V; v++) {
+                if (graph[u][v] != 0 && !set[v] && graph[u][v] < key[v]) {
                     parent[v] = u;
                     key[v] = graph[u][v];
                 }
             }
         }
-        print(parent, graph, nodes);
+        print(parent, graph);
     }
 
+    static int[][] buildAdjacencyMatrix(List<Node> nodes, List<Edge> edges) {
+        int n = nodes.size();
+        int[][] matrix = new int[n][n];
+        Map<Integer, Integer> indexMap = new HashMap<>();
 
-    static int[][] buildAdjacencyMatrix(String[] nodes, List<Edge> edges){
-        int n                       = nodes.length;
-        int[][] mtrx                = new int[n][n];
-        Map<String,Integer> idx_map = new HashMap<>();
-        for(int i=0; i<n; i++){
-            idx_map.put(nodes[i], i);
+        for (int i = 0; i < n; i++) {
+            indexMap.put(nodes.get(i).id, i);
         }
 
-        for(Edge e:edges){
-            int from = idx_map.get(e.from);
-            int to   = idx_map.get(e.to);
-            mtrx[from][to] = e.weight;
-            mtrx[to][from] = e.weight;
+        for (Edge e : edges) {
+            int from = indexMap.get(e.source);
+            int to = indexMap.get(e.target);
+            matrix[from][to] = e.weight;
+            matrix[to][from] = e.weight;
         }
-        return mtrx;
+
+        return matrix;
     }
-
 
     public static void main(String[] args) {
-        try{
-            Gson json = new Gson();
-            FileReader read = new FileReader("ass_3_input.json");
-            GraphsWrapper data = json.fromJson(read, GraphsWrapper.class);
-            PrimAlgorithm alg = new PrimAlgorithm();
+        try {
+            Gson gson = new Gson();
+            FileReader reader = new FileReader("graphs/small/smallGraph_1.json");
+            Root root = gson.fromJson(reader, Root.class);
 
-            for(Graph graph:data.graphs){
-                System.out.println(
-                        "\nGraph " + graph.id + "MST: "
-                );
-                int[][] mtrx = buildAdjacencyMatrix(graph.nodes, graph.edges);
-                alg.MST(mtrx, graph.nodes);
+            PrimAlgorithm algorithm = new PrimAlgorithm();
+            System.out.println("Loaded graph with " + root.graph.nodes.size() + " nodes and " + root.graph.edges.size() + " edges\n");
+
+            System.out.println("Input edges:");
+            for (Edge e : root.graph.edges) {
+                System.out.println(e.source + " - " + e.target + " : " + e.weight);
             }
-        }catch(Exception e){
+
+            System.out.println("\nMinimum Spanning Tree:");
+            int[][] matrix = buildAdjacencyMatrix(root.graph.nodes, root.graph.edges);
+            algorithm.MST(matrix);
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
